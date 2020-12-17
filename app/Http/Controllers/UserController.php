3d7f1ab8f25c24802e82d90a84466b28c467e3cf
Paulius Butkus <kaunaspb@gmail.com>
2020-12-17 11:04:40 +0200

    sekimas5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function show($id){
         $user = User::findOrFail($id);
         return view('users.show', ['user' => $user]);
+    }
+
+    public function follow($id){
+        $user = Auth::user();
+        $user = User::findOrFail($id);
+        $user->following()->attach($user->id);
+        return back();
+    }
+
+    public function following(){
+        $user = Auth::user();
+        $user = $user->following()->get();
+        // Pasikeisk i custom puslapi
+        //dd($user);
+        return view('users.following', ['themes' => $user]);
     }
 }
